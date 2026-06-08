b91b88ed80fd5c1fd9d8520653476519c2314282
vEmree <megaemre1@outlook.de>
2026-06-08 20:27:55 +0200

    Added layers of Crash Protection to prevent Script from crashing in different scenarios such as: [Loading into A Game], [Closing either the Discord Client or League of Legends client] Further added a clean Connection Severance Protocol on Script Termination for reusability without needing to restart the Discord client.

--- main.py
+++ main.py
@@ -5,45 +5,49 @@ from pypresence import Presence
 app_Id = "1513309358723961022"
 RPC = Presence(app_Id)
 
-RPC.connect()
-
-
-while (True):
-    try:
-        current_champion = "No Champ Selected"
-        current_level = 0
-        kills = 0
-        deaths = 0
-        assists = 0
-        cs = 0
-
-
-        response = requests.get("https://127.0.0.1:2999/liveclientdata/allgamedata", verify = False)
-
-        daten = response.json()
-
-        summoner_name = daten["activePlayer"]["riotId"]
-
-        for spieler in daten["allPlayers"]:
-            if spieler["riotId"] == summoner_name:
-                current_champion = spieler["championName"]
-                current_level = spieler["level"]
-                kills = spieler["scores"]["kills"]
-                deaths = spieler["scores"]["deaths"]
-                assists = spieler["scores"]["assists"]
-                cs = spieler["scores"]["creepScore"]
-
-        print(f'Champion: {current_champion}\nLevel: {current_level}\nKills: {kills}\nDeaths: {deaths}\nAssists: {assists}\nCS: {cs}')
-        RPC.update(state="In A Match", details="Test")
-    except requests.exceptions.ConnectionError:
+try:
+    RPC.connect()
+    while (True):
         try:
-            RPC.update(state="In Main Menu", details="Waiting for Match")
-        except Exception:
-            pass
+            current_champion = "No Champ Selected"
+            current_level = 0
+            kills = 0
+            deaths = 0
+            assists = 0
+            cs = 0
 
 
-    except Exception as randoFehler:
-        print("Entweder Discord UND ODER League geschlossen")
+            response = requests.get("https://127.0.0.1:2999/liveclientdata/allgamedata", verify = False)
+
+            daten = response.json()
+
+            summoner_name = daten["activePlayer"]["riotId"]
+
+            for spieler in daten["allPlayers"]:
+                if spieler["riotId"] == summoner_name:
+                    current_champion = spieler["championName"]
+                    current_level = spieler["level"]
+                    kills = spieler["scores"]["kills"]
+                    deaths = spieler["scores"]["deaths"]
+                    assists = spieler["scores"]["assists"]
+                    cs = spieler["scores"]["creepScore"]
+
+            print(f'Champion: {current_champion}\nLevel: {current_level}\nKills: {kills}\nDeaths: {deaths}\nAssists: {assists}\nCS: {cs}')
+            RPC.update(state=f"{current_champion} Level: {current_level}", details=f"KDA: {kills}/{deaths}/{assists} | CS: {cs}")
 
 
-    time.sleep(60)
+        except requests.exceptions.ConnectionError:
+            RPC.update(state="Waiting for a Game")
+            print("Waiting for a League Game")
+
+        except KeyError:
+            print("Game is Loading (No Playerdata available)")
+        time.sleep(5)
+
+except KeyboardInterrupt:
+    print("\nScript Terminated")
+
+finally:
+    print("Cleaning Discord Connection")
+    RPC.clear()
+    RPC.close()
